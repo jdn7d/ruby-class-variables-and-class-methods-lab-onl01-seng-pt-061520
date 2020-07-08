766d8fed0eaa5f570b62b5ddea119ec7e3be1bff
Jessie Donovan <donovanajessie@gmail.com>
2020-07-08 03:37:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -25,7 +25,16 @@ class Song
     @@artists.uniq
   end
 
-  def self.genre_count
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
   end
 
   def self.genre_count
