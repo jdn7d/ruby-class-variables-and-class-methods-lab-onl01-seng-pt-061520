5c0443d94959b50172a0326382e4ffe2296d6eee
Jessie Donovan <donovanajessie@gmail.com>
2020-07-08 03:00:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,14 @@
 class Song
 
   @@song_count = 0
+    attr_accessor :name :artist :genre
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
   #  @@song_count ++
 
-  attr_accessor :name :artist :genre
+
 
   def self.count
     @@song_count
